f6cfab4852d3fa72e647991cd2c0338e756bbbdb
Lawrence Gichuki <lawrencegichuki@gmail.com>
2020-01-26 14:16:15 +0300

    Apply link suggestions

--- app/src/main/java/com/udacity/gradle/stepapp/StepAppWidgetProvider.java
+++ app/src/main/java/com/udacity/gradle/stepapp/StepAppWidgetProvider.java
@@ -1,6 +1,5 @@
 package com.udacity.gradle.stepapp;
 
-import android.app.Activity;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
